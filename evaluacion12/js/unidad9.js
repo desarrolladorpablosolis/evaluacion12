ce4cef0e439d2a431c35a4029f28810327c122eb
desarrolladorpablosolis <118061284+desarrolladorpablosolis@users.noreply.github.com>
2022-11-12 11:46:14 -0300

    Add files via upload

---
+++ evaluacion12/js/unidad9.js
@@ -0,0 +1,438 @@
+//variables de cajas que se mostraran con js
+var cajaTxtA = document.getElementById('EA')
+var cajaTxtB = document.getElementById('EB')
+var cajaTxtC = document.getElementById('EC')
+var cajaTxtD = document.getElementById('ED')
+var cajaTxtE = document.getElementById('EE')
+var cajaTxtF = document.getElementById('EF')
+var cajaTxtG = document.getElementById('EG')
+var cajaTxtH = document.getElementById('EH')
+var cajaTxtI = document.getElementById('EI')
+var cajaTxtJ = document.getElementById('EJ')
+var cajaTxtK = document.getElementById('EK')
+var cajaTxtL = document.getElementById('EL')
+var cajaTxtM = document.getElementById('EM')
+var cajaTxtN = document.getElementById('EN')
+var cajaTxtÑ = document.getElementById('EÑ')
+var cajaTxtO = document.getElementById('EO')
+var cajaTxtP = document.getElementById('EP')
+var cajaTxtQ = document.getElementById('EQ')
+var cajaTxtR = document.getElementById('ER')
+var cajaTxtS = document.getElementById('ES')
+var cajaTxtT = document.getElementById('ET')
+var cajaTxtU = document.getElementById('EU')
+var cajaTxtV = document.getElementById('EV')
+var cajaTxtW = document.getElementById('EW')
+var cajaTxtX = document.getElementById('EX')
+var cajaTxtY = document.getElementById('EY')
+var cajaTxtZ = document.getElementById('EZ')
+//variable de caja seccion3
+var cajasecc = document.getElementById('seccion3')
+//variables de cajas que se mostraran con js
+var cajah1A = document.getElementById('h1EA')
+var cajah1B = document.getElementById('h1EB')
+var cajah1C = document.getElementById('h1EC')
+var cajah1D = document.getElementById('h1ED')
+var cajah1E = document.getElementById('h1EE')
+var cajah1F = document.getElementById('h1EF')
+var cajah1G = document.getElementById('h1EG')
+var cajah1H = document.getElementById('h1EH')
+var cajah1I = document.getElementById('h1EI')
+var cajah1J = document.getElementById('h1EJ')
+var cajah1K = document.getElementById('h1EK')
+var cajah1L = document.getElementById('h1EL')
+var cajah1M = document.getElementById('h1EM')
+var cajah1N = document.getElementById('h1EN')
+var cajah1Ñ = document.getElementById('h1EÑ')
+var cajah1O = document.getElementById('h1EO')
+var cajah1P = document.getElementById('h1EP')
+var cajah1Q = document.getElementById('h1EQ')
+var cajah1R = document.getElementById('h1ER')
+var cajah1S = document.getElementById('h1ES')
+var cajah1T = document.getElementById('h1ET')
+var cajah1U = document.getElementById('h1EU')
+var cajah1V = document.getElementById('h1EV')
+var cajah1W = document.getElementById('h1EW')
+var cajah1X = document.getElementById('h1EX')
+var cajah1Y = document.getElementById('h1EY')
+var cajah1Z = document.getElementById('h1EZ')
+//variables de cajas que se mostraran con js
+var cajaP1EA = document.getElementById('P1EA')
+var cajaP1EB = document.getElementById('P1EB')
+var cajaP1EC = document.getElementById('P1EC')
+var cajaP1ED = document.getElementById('P1ED')
+var cajaP1EE = document.getElementById('P1EE')
+var cajaP1EF = document.getElementById('P1EF')
+var cajaP1EG = document.getElementById('P1EG')
+var cajaP1EH = document.getElementById('P1EH')
+var cajaP1EI = document.getElementById('P1EI')
+var cajaP1EJ = document.getElementById('P1EJ')
+var cajaP1EK = document.getElementById('P1EK')
+var cajaP1EL = document.getElementById('P1EL')
+var cajaP1EM = document.getElementById('P1EM')
+var cajaP1EN = document.getElementById('P1EN')
+var cajaP1EÑ = document.getElementById('P1EÑ')
+var cajaP1EO = document.getElementById('P1EO')
+var cajaP1EP = document.getElementById('P1EP')
+var cajaP1EQ = document.getElementById('P1EQ')
+var cajaP1ER = document.getElementById('P1ER')
+var cajaP1ES = document.getElementById('P1ES')
+var cajaP1ET = document.getElementById('P1ET')
+var cajaP1EU = document.getElementById('P1EU')
+var cajaP1EV = document.getElementById('P1EV')
+var cajaP1EW = document.getElementById('P1EW')
+var cajaP1EX = document.getElementById('P1EX')
+var cajaP1EY = document.getElementById('P1EY')
+var cajaP1EZ = document.getElementById('P1EZ')
+//variables de cajas que se mostraran con js
+var cajaP2EA = document.getElementById('P2EA')
+var cajaP2EB = document.getElementById('P2EB')
+var cajaP2EC = document.getElementById('P2EC')
+var cajaP2ED = document.getElementById('P2ED')
+var cajaP2EE = document.getElementById('P2EE')
+var cajaP2EF = document.getElementById('P2EF')
+var cajaP2EG = document.getElementById('P2EG')
+var cajaP2EH = document.getElementById('P2EH')
+var cajaP2EI = document.getElementById('P2EI')
+var cajaP2EJ = document.getElementById('P2EJ')
+var cajaP2EK = document.getElementById('P2EK')
+var cajaP2EL = document.getElementById('P2EL')
+var cajaP2EM = document.getElementById('P2EM')
+var cajaP2EN = document.getElementById('P2EN')
+var cajaP2EÑ = document.getElementById('P2EÑ')
+var cajaP2EO = document.getElementById('P2EO')
+var cajaP2EP = document.getElementById('P2EP')
+var cajaP2EQ = document.getElementById('P2EQ')
+var cajaP2ER = document.getElementById('P2ER')
+var cajaP2ES = document.getElementById('P2ES')
+var cajaP2ET = document.getElementById('P2ET')
+var cajaP2EU = document.getElementById('P2EU')
+var cajaP2EV = document.getElementById('P2EV')
+var cajaP2EW = document.getElementById('P2EW')
+var cajaP2EX = document.getElementById('P2EX')
+var cajaP2EY = document.getElementById('P2EY')
+var cajaP2EZ = document.getElementById('P2EZ')
+
+
+
+function mostrarTODO(){
+
+	cajasecc.style.height=  '1000px'
+
+	cajaTxtA.style.display= 'none'
+	cajah1A.style.display= 'none'
+	cajaP1EA.style.display='none'
+	cajaP2EA.style.display= 'none'
+
+	cajaTxtB.style.display= 'none'
+	cajah1B.style.display= 'none'
+	cajaP1EB.style.display='none'
+	cajaP2EB.style.display= 'none'
+
+	cajaTxtC.style.display= 'none'
+	cajah1C.style.display= 'none'
+	cajaP1EC.style.display='none'
+	cajaP2EC.style.display= 'none'
+
+	cajaTxtD.style.display= 'none'
+	cajah1D.style.display= 'none'
+	cajaP1ED.style.display='none'
+	cajaP2ED.style.display= 'none'
+
+	cajaTxtE.style.display= 'none'
+	cajah1E.style.display= 'none'
+	cajaP1EE.style.display='none'
+	cajaP2EE.style.display= 'none'
+
+	cajaTxtF.style.display= 'none'
+	cajah1F.style.display= 'none'
+	cajaP1EF.style.display='none'
+	cajaP2EF.style.display= 'none'
+
+	cajaTxtG.style.display= 'none'
+	cajah1G.style.display= 'none'
+	cajaP1EG.style.display='none'
+	cajaP2EG.style.display= 'none'
+
+	cajaTxtH.style.display= 'none'
+	cajah1H.style.display= 'none'
+	cajaP1EH.style.display='none'
+	cajaP2EH.style.display= 'none'
+
+	cajaTxtI.style.display= 'none'
+	cajah1I.style.display= 'none'
+	cajaP1EI.style.display='none'
+	cajaP2EI.style.display= 'none'
+
+	cajaTxtJ.style.display= 'none'
+	cajah1J.style.display= 'none'
+	cajaP1EJ.style.display='none'
+	cajaP2EJ.style.display= 'none'
+
+	cajaTxtK.style.display= 'none'
+	cajah1K.style.display= 'none'
+	cajaP1EK.style.display='none'
+	cajaP2EK.style.display= 'none'
+
+	cajaTxtL.style.display= 'none'
+	cajah1L.style.display= 'none'
+	cajaP1EL.style.display='none'
+	cajaP2EL.style.display= 'none'
+
+	cajaTxtM.style.display= 'none'
+	cajah1M.style.display= 'none'
+	cajaP1EM.style.display='none'
+	cajaP2EM.style.display= 'none'
+
+	cajaTxtN.style.display= 'none'
+	cajah1N.style.display= 'none'
+	cajaP1EN.style.display='none'
+	cajaP2EN.style.display= 'none'
+
+	cajaTxtÑ.style.display= 'none'
+	cajah1Ñ.style.display= 'none'
+	cajaP1EÑ.style.display='none'
+	cajaP2EÑ.style.display= 'none'
+
+	cajaTxtO.style.display= 'none'
+	cajah1O.style.display= 'none'
+	cajaP1EO.style.display='none'
+	cajaP2EO.style.display= 'none'
+
+	cajaTxtP.style.display= 'none'
+	cajah1P.style.display= 'none'
+	cajaP1EP.style.display='none'
+	cajaP2EP.style.display= 'none'
+
+	cajaTxtQ.style.display= 'none'
+	cajah1Q.style.display= 'none'
+	cajaP1EQ.style.display='none'
+	cajaP2EQ.style.display= 'none'
+
+	cajaTxtR.style.display= 'none'
+	cajah1R.style.display= 'none'
+	cajaP1ER.style.display='none'
+	cajaP2ER.style.display= 'none'
+
+	cajaTxtS.style.display= 'none'
+	cajah1S.style.display= 'none'
+	cajaP1ES.style.display='none'
+	cajaP2ES.style.display= 'none'
+
+	cajaTxtT.style.display= 'none'
+	cajah1T.style.display= 'none'
+	cajaP1ET.style.display='none'
+	cajaP2ET.style.display= 'none'
+
+	cajaTxtU.style.display= 'none'
+	cajah1U.style.display= 'none'
+	cajaP1EU.style.display='none'
+	cajaP2EU.style.display= 'none'
+
+	cajaTxtV.style.display= 'none'
+	cajah1V.style.display= 'none'
+	cajaP1EV.style.display='none'
+	cajaP2EV.style.display= 'none'
+
+	cajaTxtW.style.display= 'none'
+	cajah1W.style.display= 'none'
+	cajaP1EW.style.display='none'
+	cajaP2EW.style.display= 'none'
+
+	cajaTxtX.style.display= 'none'
+	cajah1X.style.display= 'none'
+	cajaP1EX.style.display='none'
+	cajaP2EX.style.display= 'none'
+
+	cajaTxtY.style.display= 'none'
+	cajah1Y.style.display= 'none'
+	cajaP1EY.style.display='none'
+	cajaP2EY.style.display= 'none'
+
+	cajaTxtZ.style.display= 'none'
+	cajah1Z.style.display= 'none'
+	cajaP1EZ.style.display='none'
+	cajaP2EZ.style.display= 'none'
+}
+
+
+function mostrarTxtA() {
+	cajaTxtA.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1A.style.display= 'block'
+	cajaP1EA.style.display='block'
+	cajaP2EA.style.display= 'block'
+}
+function mostrarTxtB() {
+	cajaTxtB.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1B.style.display= 'block'
+	cajaP1EB.style.display='block'
+	cajaP2EB.style.display= 'block'
+}
+function mostrarTxtC() {
+	cajaTxtC.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1C.style.display= 'block'
+	cajaP1EC.style.display='block'
+	cajaP2EC.style.display= 'block'
+}
+function mostrarTxtD() {
+	cajaTxtD.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1D.style.display= 'block'
+	cajaP1ED.style.display='block'
+	cajaP2ED.style.display= 'block'
+}
+function mostrarTxtE() {
+	cajaTxtE.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1E.style.display= 'block'
+	cajaP1EE.style.display='block'
+	cajaP2EE.style.display= 'block'
+}
+function mostrarTxtF() {
+	cajaTxtF.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1F.style.display= 'block'
+	cajaP1EF.style.display='block'
+	cajaP2EF.style.display= 'block'
+}
+function mostrarTxtG() {
+	cajaTxtG.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1G.style.display= 'block'
+	cajaP1EG.style.display='block'
+	cajaP2EG.style.display= 'block'
+}
+function mostrarTxtH() {
+	cajaTxtH.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1H.style.display= 'block'
+	cajaP1EH.style.display='block'
+	cajaP2EH.style.display= 'block'
+}
+function mostrarTxtI() {
+	cajaTxtI.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1I.style.display= 'block'
+	cajaP1EI.style.display='block'
+	cajaP2EI.style.display= 'block'
+}
+function mostrarTxtJ() {
+	cajaTxtJ.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1J.style.display= 'block'
+	cajaP1EJ.style.display='block'
+	cajaP2EJ.style.display= 'block'
+}
+function mostrarTxtK() {
+	cajaTxtK.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1K.style.display= 'block'
+	cajaP1EK.style.display='block'
+	cajaP2EK.style.display= 'block'
+}
+function mostrarTxtL() {
+	cajaTxtL.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1L.style.display= 'block'
+	cajaP1EL.style.display='block'
+	cajaP2EL.style.display= 'block'
+}
+function mostrarTxtM() {
+	cajaTxtM.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1M.style.display= 'block'
+	cajaP1EM.style.display='block'
+	cajaP2EM.style.display= 'block'
+}function mostrarTxtN() {
+	cajaTxtN.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1N.style.display= 'block'
+	cajaP1EN.style.display='block'
+	cajaP2EN.style.display= 'block'
+}function mostrarTxtÑ() {
+	cajaTxtÑ.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1Ñ.style.display= 'block'
+	cajaP1EÑ.style.display='block'
+	cajaP2EÑ.style.display= 'block'
+}function mostrarTxtO() {
+	cajaTxtO.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1O.style.display= 'block'
+	cajaP1EO.style.display='block'
+	cajaP2EO.style.display= 'block'
+}
+function mostrarTxtP() {
+	cajaTxtP.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1P.style.display= 'block'
+	cajaP1EP.style.display='block'
+	cajaP2EP.style.display= 'block'
+}function mostrarTxtQ() {
+	cajaTxtQ.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1Q.style.display= 'block'
+	cajaP1EQ.style.display='block'
+	cajaP2EQ.style.display= 'block'
+}function mostrarTxtR() {
+	cajaTxtR.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1R.style.display= 'block'
+	cajaP1ER.style.display='block'
+	cajaP2ER.style.display= 'block'
+}function mostrarTxtS() {
+	cajaTxtS.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1S.style.display= 'block'
+	cajaP1ES.style.display='block'
+	cajaP2ES.style.display= 'block'
+}function mostrarTxtT() {
+	cajaTxtT.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1T.style.display= 'block'
+	cajaP1ET.style.display='block'
+	cajaP2ET.style.display= 'block'
+}function mostrarTxtU() {
+	cajaTxtU.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1U.style.display= 'block'
+	cajaP1EU.style.display='block'
+	cajaP2EU.style.display= 'block'
+}function mostrarTxtV() {
+	cajaTxtV.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1V.style.display= 'block'
+	cajaP1EV.style.display='block'
+	cajaP2EV.style.display= 'block'
+}
+function mostrarTxtW() {
+	cajaTxtW.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1W.style.display= 'block'
+	cajaP1EW.style.display='block'
+	cajaP2EW.style.display= 'block'
+}
+function mostrarTxtX() {
+	cajaTxtX.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1X.style.display= 'block'
+	cajaP1EX.style.display='block'
+	cajaP2EX.style.display= 'block'
+}
+function mostrarTxtY() {
+	cajaTxtY.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1Y.style.display= 'block'
+	cajaP1EY.style.display='block'
+	cajaP2EY.style.display= 'block'
+}
+function mostrarTxtZ() {
+	cajaTxtZ.style.display= 'block'	
+	cajasecc.style.height=  'auto'
+	cajah1Z.style.display= 'block'
+	cajaP1EZ.style.display='block'
+	cajaP2EZ.style.display= 'block'
+}
